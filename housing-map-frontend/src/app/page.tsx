b264e7ab36da31ed05996e7867f2e892e641780d
waaah <cojosh90@gmail.com>
2025-06-17 17:43:24 +0800

    feat: add navbar

--- housing-map-frontend/src/app/page.tsx
+++ housing-map-frontend/src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Navbar from '@/components/ui/navbar';
 import dynamic from 'next/dynamic';
 
 const DynamicMap = dynamic(() => import('../components/map/map'), {
@@ -7,8 +8,11 @@ const DynamicMap = dynamic(() => import('../components/map/map'), {
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen">
-      <DynamicMap />
+    <div className="flex flex-col w-screen h-screen max-w-screen max-h-screen">
+      <Navbar />
+      <div className='flex-1 w-full h-full'>
+        <DynamicMap />
+      </div>
     </div>
 
   );
--- housing-map-frontend/src/components/map/map.tsx
+++ housing-map-frontend/src/components/map/map.tsx
@@ -2,7 +2,7 @@
 import { MapContainer, TileLayer } from "react-leaflet";
 
 export default function Map() {
-  return <MapContainer center={{ lat: 51.505, lng: -0.09 }} zoom={13} scrollWheelZoom={true} style={{ height: '100vh', width: '100%' }}>
+  return <MapContainer center={{ lat: 51.505, lng: -0.09 }} zoom={13} scrollWheelZoom={true} className="w-full h-full">
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
